32a35d5c54778af878db7e091f34766f1da87481
HSH-85 <gjtjdgns@gmail.com>
2025-03-04 10:18:49 +0900

    # Comment API Init.

--- src/main/java/com/my/articles/controller/ArticleController.java
+++ src/main/java/com/my/articles/controller/ArticleController.java
@@ -24,8 +24,14 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
+    @GetMapping("comment")
+    public String showComment() {
+        return "/articles/update_comment";
+    }
+
     @GetMapping({"", "/"})
     public String showAllArticles(Model model) {
+
         List<ArticleDTO> articles = articleService.getAllArticles();
         model.addAttribute("articles", articles);
         return "/articles/show_all";
